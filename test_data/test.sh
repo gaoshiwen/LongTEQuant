35758035c9c284461a21cfc8e38a425baa4c0189
shiwen gao <2022302192060@whu.edu.cn>
2026-03-14 16:13:59 +0800

    Update test.sh

--- test_data/test.sh
+++ test_data/test.sh
@@ -1,5 +1,8 @@
+wget https://hgdownload.soe.ucsc.edu/goldenPath/galGal6/bigZips/genes/galGal6.ensGene.gtf.gz
+gunzip galGal6.ensGene.gtf.gz
+
 python integrated_quantification_sam_only.py \
     -g galGal6.ensGene.gtf \
-    -t galGal6_te.gtf \
+    -t galGal6_TE.gtf \
     -l SRR13267657.sam \
     -o output
